f4517e30d3e8a42bfc4fd356f6ffe2596f3d472f
Your Name <you@example.com>
2025-10-29 23:40:14 +0800

    Add build configuration

--- steam/next.config.mjs
+++ steam/next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  output: 'export',
+  basePath: '/Steam-Workshop',  // Replace with YOUR repo name
+  images: {
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
